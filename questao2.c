90f62e9177af4dc51e7857bff42390f33c9d93ab
alysson410 <142359574+alysson410@users.noreply.github.com>
2023-09-25 16:52:49 -0300

    resolvendo_atividade2

---
+++ questao2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+int numero, soma, algarismo;
+
+printf("digite um numero:");
+scanf("%d", &numero);
+
+if (numero % 2 == 0) {
+    printf(" %d eh par\n", numero);
+}
+else {
+    printf(" %d eh impar\n", numero);
+}
+soma = 0;
+while (numero > 0) {
+    scanf(&algarismo = numero % 10;)
+    &soma = soma + algarismo;
+    &numero = numero/10;
+
+}
+printf("a soma dos algarismos eh %d\n", soma)
+
+ return 0;
+}

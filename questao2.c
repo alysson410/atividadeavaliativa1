d227c06059dd514a8760d53742ae043fca663a60
alysson410 <142359574+alysson410@users.noreply.github.com>
2023-10-01 20:34:59 -0300

    tentandoresolver

--- questao2.c
+++ questao2.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
+int main{
 
-int main(){
-int numero, soma, algarismo;
 
-printf("digite um numero:");
-scanf("%d", &numero);
-
-if (numero % 2 == 0) {
-    printf(" %d eh par\n", numero);
-}
-else {
-    printf(" %d eh impar\n", numero);
-}
-soma = 0;
-while (numero > 0) {
-    scanf(&algarismo = numero % 10;)
-    &soma = soma + algarismo;
-    &numero = numero/10;
-//sei nem oq estou fzd
-}
-printf("a soma dos algarismos eh %d\n", soma)
-
+ 
  return 0;
 }
---
+++ questao4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+int funcionario;
+   
+
+printf("insira um funcionario:\n");
+scanf("%d.\n", &funcionario);
+
+ if(funcionario == a){
+ printf("pobre %d.\n", &funcionario);
+} if else(funcionario == b){
+    printf("ricah %d.\n", &funcionario);
+} if else(funcionario == c){
+    printf("classie medie %d.\n", &funcionario);
+}
+
+/**/
+     
+   /* float salario_a = 1000.0;
+    float salario_b = 2000.0;
+    float salario_c = 3000.0;
+
+    salario_a *= 1.05;
+    salario_b *= 1.07;
+    salario_c *= 1.08;
+*/
+
+    return 0;
+}

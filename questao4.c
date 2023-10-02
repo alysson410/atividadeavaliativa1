adf4a1f59898ed34eff2828698fbbb800bb4a16b
alysson410 <142359574+alysson410@users.noreply.github.com>
2023-10-02 16:11:23 -0300

    bla

--- questao4.c
+++ questao4.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main() {
+int main(){
 float salario = 0;
 char funcionario;
 
 scanf("%c", &funcionario);
 scanf("%f", &salario);
 
- if (funcionario == 'a') {
+ if(funcionario == 'a'){
  printf("R$ %.2f", salario *= 1.05);
 } else if(funcionario == 'b'){
     printf("R$ %.2f", salario *= 1.07);

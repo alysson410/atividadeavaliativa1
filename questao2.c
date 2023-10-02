320df3314a34be5e565c6ee5eecd8069a663b281
alysson410 <142359574+alysson410@users.noreply.github.com>
2023-10-02 14:55:40 -0300

    questaodoisresolvida

--- questao2.c
+++ questao2.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
-int main{
 
+int main() {
 
- 
- return 0;
+int numero, digito, soma = 0;
+
+ scanf("%d", &numero);
+
+ if (numero % 2 == 0){
+ printf("%d eh par\n", numero);
+ } else{
+    printf("%d eh impar\n", numero);
+ }
+
+int numero_original = numero;
+
+ while(numero > 0){
+    digito = numero % 10;
+    soma += digito;
+    numero /= 10;
+ }
+  printf("a soma dos algarismos de %d eh %d\n", numero_original, soma);
+
+    return 0;
 }
